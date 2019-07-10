8d93119fc993bbc399e39c6b8e52a85ac955ea24
Lattimore Lau <sunnyjava@hotmail.com>
2019-07-10 21:48:42 +0800

    Static.java

---
+++ src/main/java/info/ziang/java/Static.java
@@ -0,0 +1,9 @@
+package info.ziang.java;
+
+public class Static{
+    private static int[] ints = new int[10];
+
+    public static void main(String[] args){
+        System.out.println(ints[0]);
+    }
+}
